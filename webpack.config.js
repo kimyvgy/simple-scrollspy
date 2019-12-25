08e046e76b8c5a41736f8948673b4188fcd2f331
Nguyen Huu Kim <kimnguyen.ict@gmail.com>
2019-12-25 17:58:20 +0700

    chore(deps): fix potetial security vulnerablilities issues
    - chore(deps): fix potetial security vulnerablilities issues
    - chore(deps): use terser-webpack-plugin instead of uglifyjs-webpack-plugin
    
    https://github.com/webpack-contrib/uglifyjs-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
-const {resolve} = require('path')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const { resolve } = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
     entry: resolve(__dirname, 'src/index.js'),
+
     output: {
         path: resolve(__dirname, 'dist'),
         filename: 'simple-scrollspy.min.js',
         library: 'scrollSpy',
         libraryTarget: 'umd'
     },
-    plugins: [
-        new UglifyJsPlugin({
-            exclude: [/\.min\.js$/gi] // skip pre-minified libs
-        })
-    ]
+
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
 }
