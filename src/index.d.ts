8d9d4da9a93ad8a6a65b49f8469c8b879db47464
johnzanussi <johnzanussi@gmail.com>
2023-12-07 22:16:48 -0500

    Add ScrollSpy class

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,35 @@
 declare module 'simple-scrollspy' {
+
     type MenuElement = string | HTMLElement;
+    type SmoothScroll = boolean | ScrollIntoViewOptions;
+
     type Options = {
-        sectionClass?: string;
-        menuActiveTarget?: string;
-        offset?: number;
-        hrefAttribute?: string;
-        activeClass?: string;
-        scrollContainer?: string;
-        smoothScroll?: boolean | ScrollIntoViewOptions,
-    };
-    export default function scrollSpy(menuElement: MenuElement, options: Options): void;
+        sectionClass: string;
+        menuActiveTarget: string;
+        offset: number;
+        hrefAttribute: string;
+        activeClass: string;
+        scrollContainer: string;
+        smoothScroll: SmoothScroll,
+        smoothScrollBehavior?: (targetElement: HTMLElement, smoothScrollOptions: SmoothScroll) => void;
+        onActive?: (activeItem: HTMLElement) => void;
+    }
+
+    class ScrollSpy {
+        menuList: HTMLElement;
+        options: Options;
+        scroller: HTMLElement;
+        sections: NodeList;
+
+        attachEventListeners(): void;
+        onClick(event: Event): void;
+        onScroll(): void;
+        scrollTo(targetElement: HTMLElement): void;
+        getMenuItemBySection(section: HTMLElement): HTMLElement;
+        getSectionInView(): HTMLElement | void;
+        setActive(activeItem: HTMLElement): void;
+        removeCurrentActive({ ignore: HTMLElement }): void;
+    }
+
+    export default function scrollSpy(menuElement: MenuElement, options: Partial<Options>): ScrollSpy;
 }
